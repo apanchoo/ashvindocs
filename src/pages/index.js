f1fcca387977d297be862bfa306520b288a7ee87
ashvin <a.panchoo@rt-iut.re>
2023-09-06 18:24:36 +0400

    broken link

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/category/r5rom11-automatisation-et-virtualization-des-reseaux">
             Consulter mes CR ici ⏱️
           </Link>
         </div>
